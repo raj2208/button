5ff3ab1046dc55690dc8ab369f887625f52043d5
raj2208 <rajkrai22@gmail.com>
2025-03-10 23:30:54 +0530

    second commit

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-gray-100 p-4">
-      <button className="bg-gradient-to-r from-red-500 to-pink-500 text-white text-7xl w-[70vw] h-[50vh] rounded-3xl font-bold shadow-2xl transition-all duration-300 transform hover:scale-105 hover:shadow-3xl active:scale-95">
-        FLAG OFF
-      </button>
-    </div>
-  );
-}
